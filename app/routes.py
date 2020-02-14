6c3eb81c90513e633190ada3cd23d6ea8a955233
Lucas Mauro <lucasmaurodesouza@gmail.com>
2020-02-14 20:40:58 -0300

    Login mock

--- app/routes.py
+++ app/routes.py
@@ -1,9 +1,32 @@
 from app import app
+from app.forms import LoginForm
+from flask import render_template, flash, redirect, url_for
+
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, world!"
+    user = {'username': 'John Doe'}
+    posts = [
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login', methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f'Login requested for user {form.username.data}, remember_me={form.remember_me.data}')
+        return redirect(url_for('/index'))
+    return render_template('login.html', title='Sign in', form=form)
 
 
 if __name__ == '__main__':
