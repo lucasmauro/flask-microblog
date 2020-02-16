7dc6c49b499480f181a293b4bc97195ca5a3d376
Lucas Mauro <lucasmaurodesouza@gmail.com>
2020-02-16 09:34:43 -0300

    Capability of following users

--- app/models.py
+++ app/models.py
@@ -6,6 +6,13 @@ from flask_login import UserMixin
 from hashlib import md5
 
 
+followers = db.Table(
+    'followers',
+    db.Column('follower_id', db.Integer, db.ForeignKey('user.id')),
+    db.Column('followed_id', db.Integer, db.ForeignKey('user.id'))
+)
+
+
 class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64), index=True, unique=True)
@@ -14,6 +21,14 @@ class User(UserMixin, db.Model):
     posts = db.relationship('Post', backref='author', lazy='dynamic')
     about_me = db.Column(db.String(140))
     last_seen = db.Column(db.DateTime, default=datetime.utcnow())
+    followed = db.relationship(
+        'User',
+        secondary=followers,
+        primaryjoin=(followers.c.follower_id == id),
+        secondaryjoin=(followers.c.followed_id == id),
+        backref=db.backref('followers', lazy='dynamic'),
+        lazy='dynamic'
+    )
 
     def set_password(self, password):
         self.password_hash = generate_password_hash(password)
@@ -24,6 +39,27 @@ class User(UserMixin, db.Model):
     def get_avatar(self, size):
         digest = md5(self.email.lower().encode('utf-8')).hexdigest()
         return f'https://www.gravatar.com/avatar/{digest}?d=identicon&s={size}'
+
+    def is_following(self, user):
+        return self.followed.filter(followers.c.followed_id == user.id).count() > 0
+
+    def follow(self, user):
+        if not self.is_following(user):
+            self.followed.append(user)
+
+    def unfollow(self, user):
+        if self.is_following(user):
+            self.followed.remove(user)
+
+    def get_followed_posts(self):
+        followed_posts = Post.query.join(
+            followers,
+            (followers.c.followed_id == Post.user_id)).filter(
+                followers.c.follower_id == self.id)
+        own_posts = Post.query.filter_by(user_id=self.id)
+        all_posts = followed_posts.union(own_posts)
+
+        return all_posts.order_by(Post.timestamp.desc())
 
     def __repr__(self):
         return f'<User {self.username}>'
